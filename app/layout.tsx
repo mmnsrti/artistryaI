551acbff43cc3511aa6f1bac4583eaeba810c57b
mmnsrti <mmnsrti.work@gmail.com>
2024-02-21 11:21:57 +0330

    added new public folder and changed the globalcss and changed the tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const inter = IBM_Plex_Sans({  subsets: ["latin"]
+const IBMPlex = IBM_Plex_Sans({  subsets: ["latin"]
  , weight:['400', '700'] ,
  variable: '--font-ibm-plex' ,
  });
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn}>{children}</body>
+      <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
     </html>
   );
 }
