8c3c95e416ba15fe98afd28db90b5cd01eac90c3
mmnsrti <mmnsrti@gmail.com>
2024-02-21 17:24:18 +0330

    new logic for sidebar

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -13,17 +13,12 @@ const SideBar = () => {
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
-          <Image
-            src="/assets/logo.png"
-            alt="logo"
-            width={180}
-            height={28}
-          />
+          <Image src="/assets/logo.png" alt="logo" width={180} height={28} />
         </Link>
         <nav className="sidebar-nav">
           {/* <SignedIn> */}
           <ul className="sidebar-nav_elements">
-            {navLinks.slice(0,6).map((link) => {
+            {navLinks.slice(0, 6).map((link) => {
               const isActive = link.route === pathName;
               return (
                 <li
@@ -35,8 +30,39 @@ const SideBar = () => {
                   } `}
                 >
                   <Link href={link.route} className="sidebar-link">
-                    <Image alt="logo" src={link.icon}  width={36} height={36}  
-                    className={`${isActive && 'brightness-200'}`}
+                    <Image
+                      alt="logo"
+                      src={link.icon}
+                      width={36}
+                      height={36}
+                      className={`${isActive && "brightness-200"}`}
+                    />
+                    <span className="sidebar-link_text">{link.label}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+
+          <ul className="sidebar-nav_elements">
+          {navLinks.slice(6).map((link) => {
+              const isActive = link.route === pathName;
+              return (
+                <li
+                  key={link.route}
+                  className={`sidebar-nav_element cursor-pointer group ${
+                    isActive
+                      ? "bg-purple-gradient text-white "
+                      : "text-gray-700"
+                  } `}
+                >
+                  <Link href={link.route} className="sidebar-link">
+                    <Image
+                      alt="logo"
+                      src={link.icon}
+                      width={36}
+                      height={36}
+                      className={`${isActive && "brightness-200"}`}
                     />
                     <span className="sidebar-link_text">{link.label}</span>
                   </Link>
@@ -44,15 +70,17 @@ const SideBar = () => {
               );
             })}
             <li className="flex-center cursor-pointer gap-2 p-4">
-                <UserButton/>
+           
+              <UserButton />
             </li>
           </ul>
+
           {/* </SignedIn> */}
 
           {/* <SignedOut> */}
-             <Button asChild className="button bg-purple-gradient bg-cover">
-                <Link href="/signin">Login</Link>
-             </Button>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/signin">Login</Link>
+          </Button>
           {/* </SignedOut> */}
         </nav>
       </div>
