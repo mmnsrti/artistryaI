5710845ae8ad408bfad1ec1209c1f1e5f4414d60
mmnsrti <mmnsrti@gmail.com>
2024-02-21 15:22:13 +0330

    added constant and new packages and sidebar

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,18 +1,57 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { navLinks } from "@/constants";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const SideBar = () => {
+  const pathName = usePathname();
   return (
-    <aside className='sidebar'>
-        <div className='flex size-full flex-col gap-4'>
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/icons/close.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
+        </Link>
+        <nav className="sidebar-nav">
+          {/* <SignedIn> */}
+          <ul className="sidebar-nav_elements">
+            {navLinks.map((link) => {
+              const isActive = link.route === pathName;
+              return (
+                <li
+                  key={link.route}
+                  className={`sidebar-nav_element cursor-pointer group ${
+                    isActive
+                      ? "bg-purple-gradient text-white "
+                      : "text-gray-700"
+                  } `}
+                >
+                  <Link href={link.route} className="sidebar-link">
+                    <Image alt="logo" src={link.icon}  width={36} height={36}  
+                    className={`${isActive && 'brightness-200'}`}
+                    />
+                    <span className="sidebar-link_text">{link.label}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          {/* </SignedIn> */}
 
-      <Link href='/' className='sidebar-logo'>
-        <Image src='/assets/icons/close.svg' alt='logo' width={180} height={28}/>
-      </Link>
-        </div>
+          {/* <SignedOut> */}
+             
+          {/* </SignedOut> */}
+        </nav>
+      </div>
     </aside>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
