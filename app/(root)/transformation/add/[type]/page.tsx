33570384b072e8e7031a0616fa4b25d7d287dcc0
mmnsrti <mmnsrti@gmail.com>
2024-02-24 14:50:10 +0330

    basic form page and basic logics for data transfer and actions

--- app/(root)/transformation/add/[type]/page.tsx
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -1,16 +1,28 @@
-import Header from '@/components/shared/Header'
-import TransformationForm from '@/components/shared/TransformationForm'
-import { transformationTypes } from '@/constants'
-import React from 'react'
-const AddTransformationTypePage = ({params:{type}}:SearchParamProps) => {
-  const transformation = transformationTypes[type]
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
   return (
     <>
-
       <Header title={transformation.title} subtitle={transformation.subTitle} />
-      <TransformationForm/>
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
     </>
-  )
-}
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,10 +1,9 @@
-"use client"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
- 
-import { Button } from "@/components/ui/button"
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,15 +12,40 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-const TransformationForm = () => {
-     
-    const formSchema = z.object({
-        username: z.string().min(2, {
-          message: "Username must be at least 2 characters.",
-        }),
-      })
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { defaultValues } from "@/constants";
+const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
+const TransformationForm = ({
+  data = null,
+  action,
+}: TransformationFormProps) => {
+  const initialValues =
+    data && action === "Update"
+      ? {
+          title: data?.title,
+          aspectRatio: data?.aspectRatio,
+          prompt: data?.prompt,
+          publicId: data?.publicId,
+          color: data?.color,
+        }
+      : defaultValues;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -44,7 +68,7 @@ const TransformationForm = () => {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default TransformationForm
+export default TransformationForm;
