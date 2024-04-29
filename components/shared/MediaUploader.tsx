386a65fd717d07fb65c09711c102b742355fddc6
mmnsrti <mmnsrti@gmail.com>
2024-04-29 18:05:37 +0330

    Add next-cloudinary package
    This commit adds the next-cloudinary package (version 6.5.1) to the project's dependencies. The package includes the following dependencies:
    
    * @cloudinary-util/types

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useToast } from "@/components/ui/use-toast"
+
+const MediaUploader = () => {
+    const { toast } = useToast()
+    return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MediaUploader
