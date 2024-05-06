7dfea47069fa6c885db481ae5075ea525f9a4a8a
mmnsrti <mmnsrti@gmail.com>
2024-05-06 11:27:38 +0330

    "Add user population to image queries and update addImage/updateImage/deleteImage functions"
    This commit updates the image.actions.ts file by adding a new function `populateUser` that is used to populate the user field

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -5,7 +5,14 @@ import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 import Image from "../database/models/image.model";
 import User from "../database/models/user.model";
+import { redirect } from "next/navigation";
 
+const populateUser = (query: any) =>
+  query.populate({
+    path: "author",
+    model: User,
+    select: "_id firstName lastName",
+  });
 export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
@@ -18,7 +25,7 @@ export async function addImage({ image, userId, path }: AddImageParams) {
       author,
     });
     revalidatePath(path);
-    return JSON.parse(JSON.stringify(image));
+    return JSON.parse(JSON.stringify(newImage));
   } catch (error) {
     handleError(error);
   }
@@ -26,6 +33,15 @@ export async function addImage({ image, userId, path }: AddImageParams) {
 export async function updateImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
+    const imagetoUpdate = await Image.findById(image._id);
+    if (!imagetoUpdate || imagetoUpdate.author.toHexString() !== userId) {
+      throw new Error("unauthorized or image not found ");
+    }
+    const updateImage = await Image.findByIdAndUpdate(
+      imagetoUpdate._id,
+      image,
+      { new: true }
+    );
     revalidatePath(path);
     return JSON.parse(JSON.stringify(image));
   } catch (error) {
@@ -35,10 +51,13 @@ export async function updateImage({ image, userId, path }: AddImageParams) {
 export async function deleteImage({ imageId }: string) {
   try {
     await connectToDatabase();
+    await Image.findByIdAndDelete(imageId);
     revalidatePath(path);
     return JSON.parse(JSON.stringify(image));
   } catch (error) {
     handleError(error);
+  } finally {
+    redirect("/");
   }
 }
 export async function getImageById({ imageId }: string) {
