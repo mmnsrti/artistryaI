83265e8afaa586a708db65cb076833669b9bf3d8
mmnsrti <mmnsrti@gmail.com>
2024-05-06 11:12:51 +0330

    "Update image.actions.ts to include additional functions and use 'use server' directive; add imports for Image and User models"
    This commit updates the 'image.actions.ts' file to include new functions (updateImage,

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -1,20 +1,52 @@
-"use client"
+"use server";
 
-import { revalidatePath } from "next/cache"
-import { connectToDatabase } from "../database/mongoose"
-import { handleError } from "../utils"
+import { revalidatePath } from "next/cache";
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
+import Image from "../database/models/image.model";
+import User from "../database/models/user.model";
 
-export async function addImage({image,userId,path}:AddImageParams){
-    try {
-        await connectToDatabase()
-        revalidatePath
-        const imageModel = new ImageModel({
-            image,
-            userId,
-            path
-        })
-        
-    } catch (error) {
-        handleError(error)
+export async function addImage({ image, userId, path }: AddImageParams) {
+  try {
+    await connectToDatabase();
+    const author = await User.findById(userId);
+    if (!author) {
+      throw new Error("user not found");
     }
+    const newImage = await Image.create({
+      ...image,
+      author,
+    });
+    revalidatePath(path);
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+export async function updateImage({ image, userId, path }: AddImageParams) {
+  try {
+    await connectToDatabase();
+    revalidatePath(path);
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+export async function deleteImage({ imageId }: string) {
+  try {
+    await connectToDatabase();
+    revalidatePath(path);
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+export async function getImageById({ imageId }: string) {
+  try {
+    await connectToDatabase();
+    revalidatePath(path);
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
 }
