4ccba32627a4868a97d272d18d000df35aaf894b
mmnsrti <mmnsrti@gmail.com>
2024-05-11 10:35:43 +0330

    "Update image.actions.ts to include getAllImages function with query parameters; update Home page to use new function"
    This commit updates the `image.actions.ts` file to include a new `getAllImages` function that

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
-// import { getAllImages } from "@/lib/actions/image.actions"
+import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -8,7 +8,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
-  // const images = await getAllImages({ page, searchQuery})
+  const images = await getAllImages({ page, searchQuery})
 
   return (
     <>
