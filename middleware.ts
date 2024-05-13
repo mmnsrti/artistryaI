97cfaae4eeb5432254b502729bc48e0fdd56c013
mmnsrti <mmnsrti@gmail.com>
2024-05-13 10:32:29 +0330

    Update authMiddleware configuration: change public routes to ignored routes
    The `publicRoutes` array has been replaced with a `ignoredRoutes` array in the `authMiddleware` configuration. This change will ignore the specified routes for authentication,

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/","/api/webhooks/clerk","/api/webhooks/stripe"],
-  
+  ignoredRoutes: ["/api/webhooks(.*)"], 
 });
 
 export const config = {
